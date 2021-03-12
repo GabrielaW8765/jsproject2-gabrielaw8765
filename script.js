5a148523e6c1e73ce98da5b6aefd4fda1c4914b3
Gabriela Wong <GabrielaW8765@hstat.org>
2021-03-12 15:14:10 +0000

    project???

--- script.js
+++ script.js
@@ -10,22 +10,22 @@ alert ("Hey Howdy Hey! Welcome to the CATS Permits Environment Data!"); //intro
 var whatBor = prompt("What borough do you want CATS data from?");// make  choices available for certain cities
 
 
-function userLangu() {
+function userProm() {
   var catVar = [];
   var answer = whatBor;
   for (i = 0; i < data.length; i++) {
-    if(data[i].language_classes == undefined){
+    if(data[i].whatBor_boroughs == undefined){
       catVar.push("None".split(", "))
       }
   else{
-    catVar.push(data[i].language_classes.split(", "));
+    catVar.push(data[i].whatBor_boroughs.split(", "));
   }
 }
 document.write("The CATS data in " + answer  + " have " + catVar + ": <br>");
 for (i = 0; i < data.length; i++) {
-  for(var j = 0; j < catVa[i].length; j++){
-  if (catVar[i][j] == whatBor && data[i].city == whatCity.toUpperCase()){
-document.write(data[i].school_name + "<br> ")//Searches for schools with languages that the user wants to take.
+  for(var j = 0; j < catVar[i].length; j++){
+  if (catVar[i][j] == whatBor && data[i].city == whatBor.toUpperCase()){
+document.write(data[i].cats_name + "<br> ")//Searches for CATS with borough that the user wants to take.
       }
     }
   }
